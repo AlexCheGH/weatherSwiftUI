7fe65f532148ba3c3aa0fe9afe1bda9a64a16892
AlexCheGH <chekushkinSasha@gmail.com>
2021-04-25 19:10:35 +0300

    fixed network calls

--- SwiftUIWeatherApp/WeatherAppSwiftUIWatch Extension/SwiftUIWeatherAppApp.swift
+++ SwiftUIWeatherApp/WeatherAppSwiftUIWatch Extension/SwiftUIWeatherAppApp.swift
@@ -8,15 +8,49 @@
 import SwiftUI
 
 @main
-struct SwiftUIWeatherAppApp: App {    
+struct SwiftUIWeatherAppApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    
+    @ObservedObject var viewModel = WeatherViewModel()
+    
+    @State private var initialDate = Date()
+    
     @SceneBuilder var body: some Scene {
         WindowGroup {
             TabView {
-                WeatherView()
+                WeatherView(viewModel: viewModel)
+                    .onChange(of: scenePhase) { (phase) in
+                        processScenePhase(for: phase)
+                    }
+                    .onAppear { viewModel.loadData() }
+                
                 SettingsView()
             }
         }
-        
         WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+    
+    private func processScenePhase(for phase: ScenePhase) {
+        switch phase {
+        case .active:
+            loadData()
+            print("The app went in active phase")
+        case .background:
+            print("The app went in background phase")
+        case .inactive:
+            print("The app went in inactive phase")
+        @unknown default:
+            print("Unknown app state")
+        }
+    }
+    
+    private func loadData() {
+        let currentDate = Date()
+        let oneHour = 60.0 * 60.0
+        //loads data once per hour
+        if initialDate.addingTimeInterval(oneHour) < currentDate {
+            viewModel.loadData()
+            initialDate.addTimeInterval(oneHour)
+        }
     }
 }
